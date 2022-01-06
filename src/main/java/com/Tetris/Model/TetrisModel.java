7c157037d92439945a6dcc54332b414833f9f83d
Inocxh <Inocxh@gmail.com>
2022-01-06 09:43:41 +0100

    changed

--- src/main/java/com/Tetris/Model/TetrisModel.java
+++ src/main/java/com/Tetris/Model/TetrisModel.java
@@ -1,4 +1,11 @@
 package com.Tetris.Model;
+import org.jspace.SequentialSpace;
+import org.jspace.Space;
 
 public class TetrisModel {
+    private boolean[][] GameBoard;
+
+    public static void main(String[] args) {
+
+    }
 }
