64bf36c065f4ff0df020a2cd7cd16f03c664580c
luppilau <cl@estaid.dk>
2022-01-07 13:36:59 +0100

    wip

--- src/main/java/com/App/App.java
+++ src/main/java/com/App/App.java
@@ -1,22 +1,13 @@
 package com.App;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
-import com.Tetris.Model.TetrisInstance;
-import com.Tetris.Model.TetrisModel;
-import com.Tetris.TetrisCanvas.TetrisCanvas;
+import com.Tetris.TetrisCanvas.TetrisScene;
 
 /**
  * JavaFX App
@@ -25,38 +16,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        TetrisCanvas canvas = new TetrisCanvas();
-        TetrisModel game = new TetrisModel();
-        TetrisInstance instance = new TetrisInstance(canvas, game);
-
-        instance.start();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("TetrisLayout.fxml"));
-        BorderPane layout = (BorderPane) root.lookup("#layout");
-        layout.setCenter(canvas.getCanvas());
-
-        Scene scene = new Scene(root);
-        scene.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent key) -> {
-            if (key.getCode().equals(KeyCode.DOWN)) {
-                instance.moveDown();
-            } else if (key.getCode().equals(KeyCode.LEFT)) {
-                instance.moveLeft();
-            } else if (key.getCode().equals(KeyCode.RIGHT)) {
-                instance.moveRight();
-            } else if (key.getCode().equals(KeyCode.UP) ||
-                    key.getCode().equals(KeyCode.X)) {
-                instance.rotateRight();
-            } else if (key.getCode().equals(KeyCode.Z) ||
-                    key.getCode().equals(KeyCode.CONTROL)) {
-                instance.rotateLeft();
-            } else if (key.getCode().equals(KeyCode.SPACE)) {
-                instance.dropDown();
-            } else if (key.getCode().equals(KeyCode.SHIFT)) {
-                instance.swap();
-            }
-        });
-        stage.setScene(scene);
+        // HomeScreen home = new HomeScreen();
+        Parent tetrisRoot = FXMLLoader.load(getClass().getClassLoader().getResource("TetrisLayout.fxml"));
+        TetrisScene tetrisScene = new TetrisScene(tetrisRoot);
+        stage.setScene(tetrisScene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
--- src/main/java/com/Tetris/TetrisCanvas/TetrisScene.java
+++ src/main/java/com/Tetris/TetrisCanvas/TetrisScene.java
@@ -1,5 +1,44 @@
 package com.Tetris.TetrisCanvas;
 
-public class TetrisScene {
+import com.Tetris.Model.TetrisInstance;
+import com.Tetris.Model.TetrisModel;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+
+public class TetrisScene extends Scene {
+    public TetrisScene(Parent root) {
+        super(root);
+        TetrisCanvas canvas = new TetrisCanvas();
+        TetrisModel game = new TetrisModel();
+        TetrisInstance instance = new TetrisInstance(canvas, game);
+        instance.start();
+
+        BorderPane layout = (BorderPane) root.lookup("#layout");
+        layout.setCenter(canvas.getCanvas());
+
+        this.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent key) -> {
+            if (key.getCode().equals(KeyCode.DOWN)) {
+                instance.moveDown();
+            } else if (key.getCode().equals(KeyCode.LEFT)) {
+                instance.moveLeft();
+            } else if (key.getCode().equals(KeyCode.RIGHT)) {
+                instance.moveRight();
+            } else if (key.getCode().equals(KeyCode.UP) ||
+                    key.getCode().equals(KeyCode.X)) {
+                instance.rotateRight();
+            } else if (key.getCode().equals(KeyCode.Z) ||
+                    key.getCode().equals(KeyCode.CONTROL)) {
+                instance.rotateLeft();
+            } else if (key.getCode().equals(KeyCode.SPACE)) {
+                instance.dropDown();
+            } else if (key.getCode().equals(KeyCode.SHIFT)) {
+                instance.swap();
+            }
+        });
+    }
 
 }
